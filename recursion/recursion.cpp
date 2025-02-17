5c9ff8ed877f7289b5541031102b7ef53c4d4c45
TCrawley11 <tyfifago@gmail.com>
2025-02-17 15:30:31 -0500

    Recurion Practice

---
+++ recursion/recursion.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n == 1) {
+        return 0;
+    } else if (n == 2) {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main() {
+    int f;
+    std::cout << "WHich Nth value of the fibonacci sequence do u want to see?" << '\n';
+    std:: cin >> f; 
+    std::cout << fibonacci(f) << "\n";
+}
