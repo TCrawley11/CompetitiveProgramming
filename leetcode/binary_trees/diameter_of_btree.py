9868a18a479a6552fc67ad1084768bf56f678faf
TCrawley11 <tcrawley@uwo.ca>
2025-05-31 17:01:40 -0400

    543. diameter of binary tree

--- leetcode/binary_trees/diameter_of_btree.py
+++ leetcode/binary_trees/diameter_of_btree.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,3 +10,18 @@ class TreeNode:
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            l = dfs(root.left)
+            r = dfs(root.right)
+
+            nonlocal res
+            res = max(res, l + r)
+            return 1 + max(l, r)
+
+        dfs(root)
+        return res
